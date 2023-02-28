fa9ffda197b8e4d59f462c1a15cbc0c6e04796b3
Dima <100461099+dim3lagin@users.noreply.github.com>
2023-02-28 19:04:13 +0300

    Add files via upload

--- FlappyBird/bird.js
+++ FlappyBird/bird.js
@@ -7,14 +7,14 @@ bird.src = "images/bird.png";
 let pipeBottom = document.createElement('img');
 pipeBottom.src = "images/pipeBottom.png";
 
-
 let pipeUp = document.createElement('img');
 pipeUp.src = "images/pipeUP.png";
 
 let bg = document.createElement('img');
 bg.src = "images/bg.png";
-ctx.drawImage(bg, 0, 0);
 
+let fg = document.createElement('img');
+fg.src = "images/fg.png"
 
 let xPos = 50;
 let yPos = 250;
@@ -29,25 +29,28 @@ let pipes_y = [0, -100];
 let grav = 0.3;
 let change = 5;
 
+let other = 62;
 function draw() {
-    ctx.drawImage(bg, 0, 0);
     ctx.drawImage(bird, xPos, yPos);
     xPos = xPos + 1;
     yPos = yPos - change;
     change = change - grav;
 
     ctx.drawImage(bg, 0, 0);
+
     for (i = 0; i < pipes_x.length; i++) {
         ctx.drawImage(pipeUp, pipes_x[i], pipes_y[i]);
         ctx.drawImage(pipeBottom, pipes_x[i], pipes_y[i] + pipeUp.height + gap);
         pipes_x[i] = pipes_x[i] - 2;
-        rez = Math.floor(Math.random() * pipeUp.height) - pipeUp.height;
+        /*rez = Math.floor(Math.random() * pipeUp.height) - pipeUp.height;*/
+        random = 100;
 
-        /*if (pipes_x[i] < 50) {
-            ctx.drawImage(pipeBottom, pipes_x[i] + 250, pipes_y[i] + rez);
-            ctx.drawImage(pipeUp, pipes_x[i] + 250, pipes_y[i] + rez);
-        }*/
+        if (pipes_x[i] < 50) {
+            ctx.drawImage(pipeUp, pipes_x[i] + 300, pipes_y[i]);
+            ctx.drawImage(pipeBottom, pipes_x[i] + 300, pipes_y[i] + pipeUp.height + gap + random);
+        }
     }
+    ctx.drawImage(fg, 0, 450)
     ctx.drawImage(bird, xPos, yPos);
 
     requestAnimationFrame(draw);
@@ -56,15 +59,9 @@ draw();
 
 window.onkeypress = function (e) {
     if (e.keyCode == 32) {
-       
+        console.log(1)
     }
     else {
         console.log(0)
     }
 };
-
-
-
-
-
-/**/
