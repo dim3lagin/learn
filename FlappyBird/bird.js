31d8ddb8be14876082cad1438bbcba5e9637df12
Dima <100461099+dim3lagin@users.noreply.github.com>
2023-02-28 05:53:45 +0300

    Add files via upload

--- FlappyBird/bird.js
+++ FlappyBird/bird.js
@@ -26,27 +26,45 @@ let y = 0;
 let pipes_x = [cvs.width, cvs.width + 150];
 let pipes_y = [0, -100];
 
+let grav = 0.3;
+let change = 5;
+
 function draw() {
     ctx.drawImage(bg, 0, 0);
     ctx.drawImage(bird, xPos, yPos);
     xPos = xPos + 1;
+    yPos = yPos - change;
+    change = change - grav;
 
     ctx.drawImage(bg, 0, 0);
     for (i = 0; i < pipes_x.length; i++) {
         ctx.drawImage(pipeUp, pipes_x[i], pipes_y[i]);
         ctx.drawImage(pipeBottom, pipes_x[i], pipes_y[i] + pipeUp.height + gap);
         pipes_x[i] = pipes_x[i] - 2;
-        if (pipes_x[i] < 50) {
-            rez = Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-            ctx.drawImage(pipeUp, pipes_x + 50, pipes_y + rez);
-            ctx.drawImage(pipeBottom, pipes_x + 50, pipes_y + 50 + pipeUp.height + gap);
-        }
+        rez = Math.floor(Math.random() * pipeUp.height) - pipeUp.height;
+
+        /*if (pipes_x[i] < 50) {
+            ctx.drawImage(pipeBottom, pipes_x[i] + 250, pipes_y[i] + rez);
+            ctx.drawImage(pipeUp, pipes_x[i] + 250, pipes_y[i] + rez);
+        }*/
     }
     ctx.drawImage(bird, xPos, yPos);
 
     requestAnimationFrame(draw);
 }
 draw();
+
+window.onkeypress = function (e) {
+    if (e.keyCode == 32) {
+       
+    }
+    else {
+        console.log(0)
+    }
+};
+
+
+
 
 
 /**/
